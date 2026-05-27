6de539bef93a3c25f97ae950eaeffe5ce5bcc7f2
Arun <aruunsomesh@gmail.com>
2026-05-27 13:53:12 +0530

    Fix ALLOWED_HOSTS for Vercel deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']  # Update this to your specific domain in production
 
 
 # Application definition
